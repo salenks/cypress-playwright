8e3efe2dfa5d5c6125b099173fa8883de710a284
Sasa Zdravkovic <salenks@gmail.com>
2026-04-05 19:00:22 -0400

    updates cypress.config with reporter requirement

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
 
     baseUrl: 'https://www.saucedemo.com',
